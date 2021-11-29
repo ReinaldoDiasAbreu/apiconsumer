3236815d027fac5e8a12bd50789cd2182f6dbeaa
Reinaldo Junio Dias de Abreu <reinaldodiasabreu@gmail.com>
2021-11-29 16:13:42 -0300

    add error page

--- api/views.py
+++ api/views.py
@@ -3,29 +3,31 @@ import json, requests
 
 
 def home(request):
-    response = requests.get("https://api.chucknorris.io/jokes/random")
-    json_data = json.loads(response.text)
-    data = {
-        'frase': json_data,
-    }
-    return render(request, 'api/home.html', data)
+    try:
+        response = requests.get("https://api.chucknorris.io/jokes/random")
+        json_data = json.loads(response.text)
+        data = {
+            'frase': json_data,
+        }
+        return render(request, 'api/home.html', data)
+    except:
+        return render(request, 'api/erro.html')
 
 
 def search(request):
-    data = {}
-    data['total'] = 0
-    form = request.GET
-    if form and form['key'] != "":
-        link = "https://api.chucknorris.io/jokes/search?query="+form['key']
-        response = requests.get(link)
-        json_data = json.loads(response.text)
-        data['total'] = json_data['total']
-        data['busca'] = json_data['result']
-    else:
-        if form and data['total'] == 0:
-            data['erro'] = "Unfortunately not found!"
-        else:
-            data['erro'] = ""
-
-    return render(request, 'api/search.html', data)
-
+    try:
+        data = {}
+        data['total'] = -1
+        data['erro'] = ""
+        form = request.GET
+        if form and form['key'] != "":
+            link = "https://api.chucknorris.io/jokes/search?query="+form['key']
+            response = requests.get(link)
+            json_data = json.loads(response.text)
+            data['total'] = json_data['total']
+            data['busca'] = json_data['result']
+            if data['total'] == 0:
+                data['erro'] = "Unfortunately not found!"
+        return render(request, 'api/search.html', data)
+    except:
+        return render(request, 'api/erro.html')
